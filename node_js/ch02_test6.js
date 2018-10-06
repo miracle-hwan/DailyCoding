d9d3c2c22e72b1b4cde104f06f845b20a2194159
miraclehwan <miraclehwan@gmail.com>
2018-10-06 23:38:49 +0900

    20181006_node_study

---
+++ node_js/ch02_test6.js
@@ -0,0 +1,4 @@
+var nconf = require('nconf');
+nconf.env();
+
+console.log('OS 환경변수의 값 : %s', nconf.get('OS'));
---
+++ node_js/ch03_test2.js
@@ -0,0 +1,7 @@
+var Person = {};
+
+Person['age'] = 20;
+Person['name'] = '소녀시대';
+Person.mobile = '010-1234-5678';
+
+console.log('%s, %d, %s', Person.name, Person['age'], Person['mobile']);
